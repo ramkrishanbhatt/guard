6162c99d4f70aa995fa7fd76e6041d71004e8d86
kunalkr06 <98419557+kunalkr06@users.noreply.github.com>
2024-07-29 18:42:08 +0530

    Update components.Style.ts

--- guard-ui/src/components/components.Style.ts
+++ guard-ui/src/components/components.Style.ts
@@ -1,4 +1,4 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { style } from "@mui/system";
 
@@ -30,4 +30,31 @@ export const StyledBoxDisplay = styled(Box)({
     justifyContent: "center",
     width: "310px",
     height: "100px"
+  })
+
+  export const StyledShadowedStack = styled(Stack)({
+    backgroundColor: "#fff",
+    boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+    borderRadius: "8px",
+    padding: "12px",
+    display: "flex",
+    flexDirection: "column",
+    textShadow: "rgba(0,0,0,.01) 0 0 1px",
+    fontWeight: "bold",
+    alignItems: "flex-start"
+  })
+
+  export const StyledSmallTypography = styled(Box)({
+    fontSize: "16px",
+    fontWeight: 550
+  })
+
+  export const StyledMediumTypography = styled(Box)({
+    fontSize: "20px",
+    fontWeight: 600
+  })
+
+  export const StyledLargeTypography = styled(Box)({
+    fontSize: "24px",
+    fontWeight: 600
   })
