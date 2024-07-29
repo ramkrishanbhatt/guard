e6d925ed0d2e760c26222e3e82c70f6b8d0125fa
kunalkr06 <98419557+kunalkr06@users.noreply.github.com>
2024-07-30 00:48:41 +0530

    Update Frames.tsx

--- guard-ui/src/components/Frames.tsx
+++ guard-ui/src/components/Frames.tsx
@@ -1,13 +1,23 @@
 import { Box, Stack } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import ReactPlayer from "react-player";
+import { StyledShadowedStack } from "./components.Style";
 
-const Frames = ({url, times}:{url: string, times: number[]}) => {
+const Frames = ({
+  url,
+  times,
+  onClick,
+}: {
+  url: string;
+  times: number[];
+  onClick: any;
+}) => {
   const playerRef = useRef<any>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [videoUrl, setVideoUrl] = useState<string>(url);
   const [timestamps, setTimestamps] = useState<number[]>(times);
   const [frames, setFrames] = useState<string[]>([]);
+  const [selectedFrame, setSelectedFrame] = useState(0);
 
   useEffect(() => {
     const captureFrames = async () => {
@@ -19,7 +29,7 @@ const Frames = ({url, times}:{url: string, times: number[]}) => {
           if (frame) {
             capturedFrames.push(frame);
           }
-          //await delay(500); 
+          // await delay(500);
         }
         setFrames(capturedFrames);
       }
@@ -30,8 +40,7 @@ const Frames = ({url, times}:{url: string, times: number[]}) => {
   const setVideoTime = (time: number): Promise<void> => {
     return new Promise((resolve) => {
       if (playerRef.current) {
-        const player =
-          playerRef?.current?.getInternalPlayer() as any || {};
+        const player = (playerRef?.current?.getInternalPlayer() as any) || {};
         player.currentTime = time;
         player.onseeked = () => resolve();
       }
@@ -55,22 +64,19 @@ const Frames = ({url, times}:{url: string, times: number[]}) => {
     new Promise((resolve) => setTimeout(resolve, ms));
 
   return (
-    <Box
+    <StyledShadowedStack
       width={"100%"}
-      //p={1}
-      //border={"1px solid black"}
-      //mt={1}
-      ml={1}
-      height={"60vh"}
-			display={"flex"}
-			flexDirection={"row"}
+      height={"inherit"}
+      display={"flex"}
+      sx={{ flexDirection: "row" }}
+      overflow={"hidden"}
     >
       <ReactPlayer
         ref={playerRef}
         url={`http://127.0.0.1:8000${url}`}
         controls={true}
-        width="60%"//change
-        height="300px"//change
+        width="60%" //change
+        height="300px" //change
         config={{ file: { attributes: { crossOrigin: "anonymous" } } }}
       />
       <canvas
@@ -84,16 +90,31 @@ const Frames = ({url, times}:{url: string, times: number[]}) => {
         flexWrap={"wrap"}
         direction={"row"}
         width={"50%"}
-        spacing={50}
+        spacing={1}
         overflow={"auto"}
+        height={"inherit"}
+        ml={1}
       >
-        <Box padding={.5} marginRight={0.5}>{/* change */}
-          {frames.map((frame, index) => (
-            <img key={index} src={frame} alt={`Frame ${index}`} width={"135px"}/>
-          ))}
-        </Box>
+        {frames.map((frame, index) => (
+          <StyledShadowedStack
+            sx={{ p: 0, borderRadius: 0, ":hover": { cursor: "pointer" } }}
+            border={index === selectedFrame ? "3px solid #006dd9" : "none"}
+          >
+            <img
+              onClick={() => {
+                onClick(timestamps[index]);
+                setSelectedFrame(index);
+                setVideoTime(timestamps[index]);
+              }}
+              key={index}
+              src={frame}
+              alt={`Frame ${index}`}
+              width={"135px"}
+            />
+          </StyledShadowedStack>
+        ))}
       </Stack>
-    </Box>
+    </StyledShadowedStack>
   );
 };
 
