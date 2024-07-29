19207d45a6b2beff280407da0aa8a700b7d21116
kunalkr06 <98419557+kunalkr06@users.noreply.github.com>
2024-07-29 22:04:59 +0530

    Update Review.tsx

--- guard-ui/src/components/Review.tsx
+++ guard-ui/src/components/Review.tsx
@@ -1,32 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { Box, Stack, TextField, Typography, Button, Grid } from "@mui/material";
-import {StyledBoxComment, StyledBoxCommentDialog, StyledBoxDisplay, StyledBoxScore, StyledBoxTools} from "./components.Style"
+import { Box, Stack, Button, Grid } from "@mui/material";
+import { StyledBoxDisplay } from "./components.Style";
 import Frames from "./Frames";
+import ReviewDecidingGrid from "./ReviewDecidingGrid";
+import { useEffect, useState } from "react";
 
-const Review = ({ id, onClick, times }: { id: string; onClick: any, times: number[] }) => {
-
+const Review = ({
+  id,
+  onClick,
+  times,
+}: {
+  id: string;
+  onClick: any;
+  times: number[];
+}) => {
   const countTime = [
-    {text: "Processed Count", val: "451"},
-    {text: "Review Time", val: "4:23:52"}
-    ];
+    { text: "Processed Count", val: "451" },
+    { text: "Review Time", val: "4:23:52" },
+  ];
 
   const textInAiDetection = [
-    {text: "Text 1", color: "#A9D18E"},
-    {text: "Text 2", color: "#F4AB83"},
-    {text: "Text 3", color: "#D6DCE5"},
+    { text: "Text 1", color: "#A9D18E" },
+    { text: "Text 2", color: "#F4AB83" },
+    { text: "Text 3", color: "#D6DCE5" },
   ];
 
   const qualityTools = [
-    {text: "RESOLUTION:", val: "XXX"},
-    {text: "AUDIO QUALITY:", val: "XXX"},
-    {text: "XXX:", val: "XXX"}
+    { text: "RESOLUTION:", val: "XXX" },
+    { text: "AUDIO QUALITY:", val: "XXX" },
+    { text: "XXX:", val: "XXX" },
   ];
 
   const notes = [
-    {text: "Notes"},
-    {text: "Annotations"},
+    { text: "Notes" },
+    { text: "Annotations" },
     //{text: "Transfer"},
-  ]
+  ];
+
+  const rejectionText = [
+    { text: "Hate Speech" },
+    { text: "Nudity" },
+    { text: "Personal Information" },
+    { text: "Copy right's violation" },
+    { text: "Bullying" },
+    { text: "Graphics" },
+    { text: "Spam" },
+    { text: "Pass to SME" },
+  ];
 
   const [url, setUrl] = useState("");
 
@@ -42,39 +61,119 @@ const Review = ({ id, onClick, times }: { id: string; onClick: any, times: numbe
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getVideoDetails();
-  },[id])
-console.log(id);
+  }, [id]);
+  console.log(id);
 
   return (
-    <Stack>
-      <Box sx={{display: "flex", justifyContent: 'flex-end'}}>
+    <Box bgcolor={"#F1FAFC"}>
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
         {countTime.map((_) => (
-          <StyledBoxDisplay m={2} p={2} color={"#002060"} fontWeight={400}>
+          <StyledBoxDisplay m={1} p={1} color={"#FFFFFF"} fontWeight={600}>
             {`${_.text} ${_.val}`}
-        </StyledBoxDisplay>
+          </StyledBoxDisplay>
         ))}
       </Box>
-      <Grid
-        container
-        rowSpacing={3}>
-        <Grid item xs={6} m={1.5} bgcolor={"lightblue"} height={"435px"}>
+      <Grid container rowSpacing={3}>
+        <Grid
+          item
+          xs={6}
+          mt={"30px"}
+          marginLeft={"10px"}
+          pt={"30px"}
+          height={"420px"}
+        >
           <Stack>
             {/* <Button onClick={onClick}>Close</Button> */}
-              <Frames url={url || ""} times={times}/>
-            </Stack>
+            <Frames url={url || ""} times={times} />
+          </Stack>
         </Grid>
-        <Grid item xs={2.9}>
+        <Grid
+          item
+          xs={5.9}
+          mt={2}
+          p={"15px"}
+          direction={"row"}
+          spacing={1}
+          borderRadius={"25px"}
+        >
+          <Grid container spacing={{ md: 0.5 }}>
+            <Grid item md={5.9} /* border={"1px solid black"} */ mr={1.5}>
+              <ReviewDecidingGrid
+                text="High confidence score"
+                data={rejectionText}
+                required={false}
+              />
+            </Grid>
+            <Grid item md={5.9}>
+              <ReviewDecidingGrid
+                text="Medium confidence score"
+                data={rejectionText}
+                required={false}
+              />
+            </Grid>
+            <Grid item md={5.9} mt={"16px"} mr={1.5}>
+              <ReviewDecidingGrid
+                text="Low confidence score"
+                data={rejectionText}
+                required={false}
+              />
+            </Grid>
+            <Grid item md={5.9} mt={"16px"}>
+              <ReviewDecidingGrid
+                text="Rejection or Approval space"
+                data={rejectionText}
+                required={false}
+              />
+              <Grid mt={"16px"} spacing={{ md: 0.5 }}>
+                <Button
+                  sx={{
+                    backgroundColor: "#405BBA",
+                    color: "#FFFFFF",
+                    padding: "10px",
+                    fontWeight: 500,
+                    borderRadius: "10px",
+                    ":hover": { bgcolor: "#405BBA" },
+                  }}
+                >
+                  Approve
+                </Button>
+                <Button
+                  sx={{
+                    backgroundColor: "#FF0054",
+                    color: "#FFFFFF",
+                    padding: "10px",
+                    fontWeight: 500,
+                    borderRadius: "10px",
+                    marginLeft: "10px",
+                    ":hover": { bgcolor: "#FF0054" },
+                  }}
+                >
+                  Reject
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Review;
+
+{
+  /* <Grid item xs={2.9}>
           <Grid container>
             <Grid item justifyContent={"center"}>
-              <StyledBoxTools m={"15px"} p={"15px"} >
-                <Typography>AI Detection</Typography>
+              <StyledBoxTools marginLeft={"15px"} p={"15px"} mt={"5px"}>
+                <Typography mt={"5px"} color={"#FFFFFF"}>AI Detection</Typography>
               </StyledBoxTools>  
             </Grid>
-            <Grid item m={"15px"} p={"15px"} bgcolor={"#B2E7FA"} justifyContent={"center"} width={"500px"} height={"325px"}>
+            <Grid item m={"15px"} p={"15px"} bgcolor={"#E3E3E9"} justifyContent={"center"} width={"330px"} height={"350px"}>
               {textInAiDetection.map((_)=>(
-                <StyledBoxScore m={"20px"} p={"20px"} bgcolor={_.color} textAlign={"center"}>
+                <StyledBoxScore m={"20px"} p={"20px"} bgcolor={_.color} textAlign={"center"} color={"#000000"}>
                   {_.text}
               </StyledBoxScore>
               ))}
@@ -83,7 +182,7 @@ console.log(id);
         </Grid>
         <Grid item xs={2.9}>
           {qualityTools.map((_)=>(
-            <StyledBoxTools m={"5px"} p={"5px"} color={"#002060"} fontWeight={"450px"}>
+            <StyledBoxTools m={"5px"} p={"5px"} color={"#FFFFFF"} fontWeight={"450px"}>
               {`${_.text} ${_.val}`}
             </StyledBoxTools>
           ))}
@@ -96,26 +195,5 @@ console.log(id);
             </StyledBoxComment>
             </>
           ))}
-          
-        </Grid>
-      </Grid>
-      <Stack m={2} p={"15px"} bgcolor={"#DAC4B6"} direction={"row"} spacing={1} borderRadius={"25px"} width={"1000px"}>
-        <Stack
-          useFlexGap
-          flexWrap={"wrap"}
-          direction={"row"}
-          width={"100%"}
-          spacing={"10px"}
-        >
-          {[1, 2, 3, 4, 5, 6].map((_) => (
-            <Box bgcolor={"#DCDDDC"} p={1} color={"#1B376F"}>Text {_}</Box>
-          ))}
-        </Stack>
-        <Button sx={{backgroundColor: "#00B0F0", color: "#FFFFFF", padding: "10px", fontWeight: 500, borderRadius: "10px"}}>Approve</Button>
-        <Button sx={{backgroundColor: "#8497B0", color: "#FFFFFF", padding: "10px", fontWeight: 500, borderRadius: "10px"}}>Reject</Button>
-      </Stack>
-    </Stack>
-  );
-};
-
-export default Review;
+        </Grid> */
+}
