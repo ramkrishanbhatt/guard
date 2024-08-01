42abedbcccc079d4bc54ed90e0f31405efe0e348
kunalkr06 <98419557+kunalkr06@users.noreply.github.com>
2024-08-01 22:37:14 +0530

    Update Review.tsx

--- guard-ui/src/components/Review.tsx
+++ guard-ui/src/components/Review.tsx
@@ -1,89 +1,214 @@
-import { Stack } from "@mui/material";
+import { Stack, Button, Grid } from "@mui/material";
 import { StyledShadowedStack, StyledSmallTypography } from "./components.Style";
-import { convertToTitleCase } from "./Data";
+import Frames from "./Frames";
+import ReviewDecidingGrid from "./ReviewDecidingGrid";
+import { useEffect, useState } from "react";
+import { classes } from "./Data";
 
-function ReviewDecidingGrid({ data }: { data: any }) {
+const Review = ({
+  id,
+  onClick,
+  times,
+}: {
+  id: string;
+  onClick: any;
+  times: number[];
+}) => {
+  const [url, setUrl] = useState("");
+  const [output, setOutput] = useState<any[]>([]);
+  const [data, setData] = useState({
+    low: [],
+    medium: [],
+    high: [],
+    reject: [],
+  });
+  const [tags, setTags] = useState<string[]>([]);
+
+  const getClassesAndScore = (time: number) => {
+    const res = output.find((_) => _.time === time);
+    const filteredData = {
+      low: [],
+      high: [],
+      medium: [],
+      reject: [],
+    };
+
+    res?.classes.forEach((e: any) => {
+      const score = e.score;
+      const item = classes.find((_) => _.class === e.class);
+      if (score < 0.4 && score > 0.01 && item?.include === "Y") {
+        filteredData.low = filteredData.low.concat(e);
+      } else if (score <= 0.7 && score >= 0.4 && item?.include === "Y") {
+        filteredData.medium = filteredData.medium.concat(e);
+      } else if (score > 0.7 && item?.include === "Y") {
+        filteredData.high = filteredData.high.concat(e);
+      }
+    });
+
+    setData(() => filteredData);
+  };
+
+  const getVideoDetails = async () => {
+    const response = await fetch(`http://3.143.254.4/get-processed-data/${id}`);
+    if (response.ok) {
+      const videosData = await response.json();
+      setUrl(() => videosData.fileData.file_path);
+      setOutput(() => videosData.hiveResponse.status[0].response.output);
+    } else {
+      setUrl("");
+    }
+  };
+
+  const approveOrRejectContent = async (val: string) => {
+    const response = await fetch(`http://3.143.254.4/update-decision`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        video_id: id,
+        status: val,
+        classes: tags,
+      }),
+    });
+    if (response.ok) {
+      alert(val);
+      onClick();
+    }
+  };
+
+  useEffect(() => {
+    getVideoDetails();
+  }, [id]);
+
+  useEffect(() => {
+    getClassesAndScore(times[0]);
+  }, [output]);
+
   return (
-    <StyledShadowedStack
-      maxHeight={"-webkit-fill-available"}
-      overflow={"auto"}
+    <Stack
+      bgcolor={"#F1FAFC"}
       display={"flex"}
       flexDirection={"column"}
-      width={"-webkit-fill-available"}
+      height={"-webkit-fill-available"}
+      overflow={"hidden"}
+      p={1}
+      spacing={1}
     >
-      <Stack
-        width={"100%"}
-        useFlexGap
-        flexWrap={"wrap"}
-        direction={"row"}
-        spacing={1}
-        overflow={"auto"}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
-        {data.high.map((_: any) => (
-          <StyledShadowedStack
-            sx={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              p: 1,
-              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-              height: "30px",
-              alignItems: "center",
-              bgcolor: "rgb(231, 76, 60)",
-              color: "white",
-            }}
-            width={"max-content"}
-          >
-            <StyledSmallTypography alignItems={"center"}>
-              {convertToTitleCase(_.class)}{" "}
-              {`(${parseFloat((_?.score).toFixed(2)) * 100}%)`}
-            </StyledSmallTypography>
-          </StyledShadowedStack>
-        ))}
-        {data.medium.map((_: any) => (
-          <StyledShadowedStack
-            sx={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              p: 1,
-              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-              height: "30px",
-              alignItems: "center",
-              bgcolor: "rgb(243, 156, 18)",
-              color: "white",
-            }}
-            width={"max-content"}
-          >
-            <StyledSmallTypography alignItems={"center"}>
-              {convertToTitleCase(_.class)}{" "}
-              {`(${parseFloat((_?.score).toFixed(2)) * 100}%)`}
-            </StyledSmallTypography>
-          </StyledShadowedStack>
-        ))}
-        {data.low.map((_: any) => (
-          <StyledShadowedStack
-            sx={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              p: 1,
-              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-              height: "30px",
-              alignItems: "center",
-              bgcolor: "rgb(241, 196, 15)",
-              color: "white",
-            }}
-            width={"max-content"}
-          >
-            <StyledSmallTypography alignItems={"center"}>
-              {convertToTitleCase(_.class)}{" "}
-              {`(${parseFloat((_?.score).toFixed(2)) * 100}%)`}
-            </StyledSmallTypography>
-          </StyledShadowedStack>
-        ))}
+      <Stack height={"60vh"}>
+        <Frames url={url} times={times} onClick={getClassesAndScore} />
       </Stack>
-    </StyledShadowedStack>
+      <Grid
+        container
+        height={"-webkit-fill-available"}
+        overflow={"hidden"}
+        flex={1}
+        columns={12}
+      >
+        <Grid mr={1} height={"inherit"} overflow={"hidden"} item md={7}>
+          <ReviewDecidingGrid data={data} />
+        </Grid>
+        <Grid item md={4.9} display={"flex"} justifyContent={"flex-start"}>
+          <StyledShadowedStack
+            spacing={1}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "row",
+            }}
+          >
+            <Stack
+              width={"fit-content"}
+              spacing={1}
+              direction="row"
+              useFlexGap
+              flexWrap="wrap"
+            >
+              {queueData.map((_) => (
+                <StyledShadowedStack
+                  width={"130px"}
+                  sx={{
+                    py: 0.5,
+                    px: 1,
+                    borderRadius: 1,
+                    boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                    ":hover": {
+                      cursor: "pointer",
+                      bgcolor: "#006dd9",
+                      color: "white",
+                    },
+                    bgcolor: tags.includes(_) ? "#006dd9" : "white",
+                    color: tags.includes(_) ? "white" : "black",
+                  }}
+                  onClick={() =>
+                    setTags((prev) => {
+                      const data = prev.includes(_)
+                        ? prev.filter((e) => e !== _)
+                        : [...prev, _];
+                      return data;
+                    })
+                  }
+                >
+                  <StyledSmallTypography sx={{ fontSize: "12px" }}>
+                    {_}
+                  </StyledSmallTypography>
+                </StyledShadowedStack>
+              ))}
+            </Stack>
+            <Stack mt={0} direction={"row"} spacing={1}>
+              <Button
+                sx={{
+                  backgroundColor: "#405BBA",
+                  color: "#FFFFFF",
+                  padding: "10px",
+                  fontWeight: 500,
+                  borderRadius: 1,
+                  height: "100%",
+                  ":hover": { bgcolor: "#405BBA" },
+                  ":disabled": { bgcolor: "lightgray" },
+                }}
+                disabled={
+                  tags.includes("Hate Speech") ||
+                  tags.includes("Bullying") ||
+                  tags.includes("Personal Information") ||
+                  tags.includes("Nudity") ||
+                  tags.includes("Graphic Violence") ||
+                  tags.includes("Copyright Violations")
+                }
+                onClick={() => approveOrRejectContent("Approved")}
+              >
+                Approve
+              </Button>
+              <Button
+                sx={{
+                  backgroundColor: "#FF0054",
+                  color: "#FFFFFF",
+                  padding: "10px",
+                  fontWeight: 500,
+                  borderRadius: 1,
+                  ":hover": { bgcolor: "#FF0054" },
+                }}
+                onClick={() => approveOrRejectContent("Rejected")}
+              >
+                Reject
+              </Button>
+            </Stack>
+          </StyledShadowedStack>
+        </Grid>
+      </Grid>
+    </Stack>
   );
-}
+};
 
-export default ReviewDecidingGrid;
+export default Review;
+
+const queueData = [
+  "Hate Speech",
+  "Bullying",
+  "Personal Information",
+  "Spam",
+  "Nudity",
+  "Graphic Violence",
+  "Copyright Violations",
+  "Pass to SME",
+];
