f26b4a7f98722100d6da4e71cfd8a71a6b214d9b
kunalkr06 <98419557+kunalkr06@users.noreply.github.com>
2024-08-01 22:36:47 +0530

    Update ReviewDecidingGrid.tsx

--- guard-ui/src/components/ReviewDecidingGrid.tsx
+++ guard-ui/src/components/ReviewDecidingGrid.tsx
@@ -1,56 +1,18 @@
-import { Box, Stack } from "@mui/material";
-import { useState } from "react";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import {
-  StyledMediumTypography,
-  StyledShadowedStack,
-  StyledSmallTypography,
-} from "./components.Style";
+import { Stack } from "@mui/material";
+import { StyledShadowedStack, StyledSmallTypography } from "./components.Style";
 import { convertToTitleCase } from "./Data";
 
-function ReviewDecidingGrid({
-  text,
-  data,
-  required,
-}: {
-  text: string;
-  data: any[];
-  required: boolean;
-}) {
-  const [formats, setFormats] = useState(() => ["bold", "italic"]);
-
-  const handleFormat = (
-    event: React.MouseEvent<HTMLElement>,
-    newFormats: string[]
-  ) => {
-    setFormats(newFormats);
-  };
-
+function ReviewDecidingGrid({ data }: { data: any }) {
   return (
-    <Box
-      //justifyContent={"center"}
-      ml={"15px"}
-      height={"31vh"}
-      overflow={"hidden"}
+    <StyledShadowedStack
+      maxHeight={"-webkit-fill-available"}
+      overflow={"auto"}
       display={"flex"}
       flexDirection={"column"}
+      width={"-webkit-fill-available"}
     >
-      <StyledShadowedStack
-        sx={{
-          background: "#006dd9",
-          color: "white",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        mb={0.5}
-      >
-        <StyledMediumTypography alignItems={"center"}>
-          {text}
-        </StyledMediumTypography>
-      </StyledShadowedStack>
       <Stack
+        width={"100%"}
         useFlexGap
         flexWrap={"wrap"}
         direction={"row"}
@@ -59,7 +21,7 @@ function ReviewDecidingGrid({
         alignItems={"center"}
         justifyContent={"center"}
       >
-        {data.map((_) => (
+        {data.high.map((_: any) => (
           <StyledShadowedStack
             sx={{
               flexDirection: "row",
@@ -68,86 +30,59 @@ function ReviewDecidingGrid({
               boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
               height: "30px",
               alignItems: "center",
-              bgcolor:
-                text === "High confidence score"
-                  ? "rgb(231, 76, 60)"
-                  : text === "Medium confidence score"
-                  ? "rgb(243, 156, 18)"
-                  : text === "Low confidence score"
-                  ? "rgb(241, 196, 15)"
-                  : "green",
+              bgcolor: "rgb(231, 76, 60)",
               color: "white",
             }}
             width={"max-content"}
           >
             <StyledSmallTypography alignItems={"center"}>
-              {convertToTitleCase(_.class)} {`(${parseFloat((_?.score).toFixed(2))*100}%)`}
+              {convertToTitleCase(_.class)}{" "}
+              {`(${parseFloat((_?.score).toFixed(2)) * 100}%)`}
+            </StyledSmallTypography>
+          </StyledShadowedStack>
+        ))}
+        {data.medium.map((_: any) => (
+          <StyledShadowedStack
+            sx={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              p: 1,
+              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+              height: "30px",
+              alignItems: "center",
+              bgcolor: "rgb(243, 156, 18)",
+              color: "white",
+            }}
+            width={"max-content"}
+          >
+            <StyledSmallTypography alignItems={"center"}>
+              {convertToTitleCase(_.class)}{" "}
+              {`(${parseFloat((_?.score).toFixed(2)) * 100}%)`}
+            </StyledSmallTypography>
+          </StyledShadowedStack>
+        ))}
+        {data.low.map((_: any) => (
+          <StyledShadowedStack
+            sx={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              p: 1,
+              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+              height: "30px",
+              alignItems: "center",
+              bgcolor: "rgb(241, 196, 15)",
+              color: "white",
+            }}
+            width={"max-content"}
+          >
+            <StyledSmallTypography alignItems={"center"}>
+              {convertToTitleCase(_.class)}{" "}
+              {`(${parseFloat((_?.score).toFixed(2)) * 100}%)`}
             </StyledSmallTypography>
           </StyledShadowedStack>
         ))}
       </Stack>
-      {/* <Box
-        sx={{
-          backgroundColor: "#016DD9",
-          borderRadius: "5px",
-          width: "290px",
-          padding: "15px",
-          color: "#FFFFFF",
-          fontWeight: 600,
-        }}
-      >
-        {text}
-      </Box> */}
-      {/* <Stack
-        spacing={{ xs: 0.5, sm: 0.5 }}
-        direction="row"
-        useFlexGap
-        flexWrap="wrap"
-        mt={0.5}
-      >
-        {required ? (
-          <ToggleButtonGroup
-            value={formats}
-            onChange={handleFormat}
-            aria-label="text formatting"
-            orientation="horizontal"
-            sx={{
-              borderRadius: "5px",
-              flexWrap: "wrap",
-              ml: "15px",
-            }}
-          >
-            {data.map((_: any) => (
-              <ToggleButton
-                value={_.text}
-                sx={{
-                  color: "#808080",
-                  textAlign: "left",
-                  fontWeight: 600,
-                  width: "150px",
-                }}
-              >
-                {_.text}
-              </ToggleButton>
-            ))}
-          </ToggleButtonGroup>
-        ) : (
-          <>
-            {data.map((_: any) => (
-              <Box
-                bgcolor={"#FFFFFF"}
-                p={0.5}
-                width={"150px"}
-                borderRadius={"5px"}
-                sx={{ color: "#808080", textAlign: "left", fontWeight: 600 }}
-              >
-                {_.text}
-              </Box>
-            ))}
-          </>
-        )}
-      </Stack> */}
-    </Box>
+    </StyledShadowedStack>
   );
 }
 
