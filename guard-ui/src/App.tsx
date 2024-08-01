4582f96f6ebf4e56e8789b0f313a2f0a353834db
kunalkr06 <98419557+kunalkr06@users.noreply.github.com>
2024-08-01 22:33:51 +0530

    Update App.tsx

--- guard-ui/src/App.tsx
+++ guard-ui/src/App.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import UploadVideo from "./components/UploadVideo";
 import { Box, IconButton, Stack, Typography } from "@mui/material";
-import ListResults from "./components/ListResults";
 import Review from "./components/Review";
 import Logo from "./Sutherland_idAvkZdSwO_0.png";
 import { Home, Person } from "@mui/icons-material";
 import QueueDetails from "./components/QueueDetails";
-import Frames from "./components/Frames";
 import { classes } from "./components/Data";
+import { StyledBoxDisplay } from "./components/components.Style";
 
 function App() {
   const [view, setView] = useState(false);
   const [id, setId] = useState("");
   const [times, setTimes] = useState<number[]>([]);
+
+  const countTime = [
+    { text: "Processed Count", val: "451" },
+    { text: "Review Time", val: "4:23:52" },
+  ];
 
   return (
     <div className="App">
@@ -36,7 +38,7 @@ function App() {
               <img src={Logo} width={"230px"} height={"35px"} />
               <Typography fontSize={"22px"}>DefendR</Typography>{" "}
             </Stack>
-            
+
             <Typography
               alignItems={"center"}
               justifyContent={"center"}
@@ -47,33 +49,55 @@ function App() {
               <span style={{ fontWeight: 600 }}>User</span>
             </Typography>
           </Stack>
-          <Stack px={3} alignItems={"center"} direction={"row"} spacing={1}>
-            <Box
-              px={3}
-              py={0.48}
-              color={view ? "black" : "#006dd9"}
-              borderBottom={view ? "none" : "4px solid #006dd9"}
-            >
-              <IconButton
-                sx={{ p: 0.2 }}
-                color="inherit"
-                onClick={() => setView(false)}
+          <Stack
+            px={3}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            direction={"row"}
+            spacing={1}
+          >
+            <Stack direction={"row"}>
+              <Box
+                px={3}
+                py={0.48}
+                color={view ? "black" : "#006dd9"}
+                borderBottom={view ? "none" : "4px solid #006dd9"}
               >
-                <Home />
-              </IconButton>
-            </Box>
-            <Box
-              px={3}
-              py={1}
-              fontWeight={600}
-              color={view ? "#006dd9" : "black"}
-              borderBottom={!view ? "none" : "4px solid #006dd9"}
+                <IconButton
+                  sx={{ p: 0.2 }}
+                  color="inherit"
+                  onClick={() => setView(false)}
+                >
+                  <Home />
+                </IconButton>
+              </Box>
+              <Box
+                px={3}
+                py={1}
+                fontWeight={600}
+                color={view ? "#006dd9" : "black"}
+                borderBottom={!view ? "none" : "4px solid #006dd9"}
+              >
+                Review Screen
+              </Box>
+              <Box px={3} py={1} fontWeight={600} color={"black"}>
+                Moderator Monitoring
+              </Box>
+            </Stack>
+            <Stack
+              spacing={1}
+              mb={1}
+              direction={"row"}
+              sx={{
+                justifyContent: "flex-end",
+              }}
             >
-              Review Screen
-            </Box>
-            <Box px={3} py={1} fontWeight={600} color={"black"}>
-              Moderator Monitoring
-            </Box>
+              {countTime.map((_) => (
+                <StyledBoxDisplay p={1} color={"#FFFFFF"} fontWeight={600}>
+                  {`${_.text} ${_.val}`}
+                </StyledBoxDisplay>
+              ))}
+            </Stack>
           </Stack>
         </Box>
         {view ? (
@@ -89,11 +113,11 @@ function App() {
         ) : (
           <Box flex={1} p={3} mb={2} height={"inherit"} overflow={"hidden"}>
             <QueueDetails
-              classes={classes.filter((_)=>_.include==="Y")}
+              classes={classes.filter((_) => _.include === "Y")}
               onClick={(id: string, times: number[]) => {
                 setView(true);
                 setId(id);
-                setTimes(()=>times);
+                setTimes(() => times);
               }}
             />
           </Box>
